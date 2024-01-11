904d8b6a7db526e79933e04402c1c3e6f0529997
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2024-01-11 06:48:35 +0530

    Add project files.

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+        public BubbleSort() 
+        {
+            Console.WriteLine("Enter the length of an Array");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter {0} elements", n);
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            // Console.WriteLine( arr.Length);
+            for (int i = 0;i < arr.Length - 1; i++)
+            {
+                int ichange = 0;
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        ichange++;
+                    }
+                }if(ichange == 0)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class SelectionSort
+    {
+        public SelectionSort() 
+        {
+            Console.WriteLine("Enter the length of an Array");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter {0} elements", n);
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0;i < arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }if(i != min)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
